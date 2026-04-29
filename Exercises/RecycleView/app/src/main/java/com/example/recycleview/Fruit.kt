24a07015485361dba8f3dec47342106c13903277
Prathyush-RK <230701240@rajalakshmi.edu.in>
2026-04-29 19:08:33 +0530

    Recycle View

---
+++ Exercises/RecycleView/app/src/main/java/com/example/recycleview/Fruit.kt
@@ -0,0 +1,8 @@
+package com.example.recycleview
+
+// A data class automatically generates:
+// equals(), hashCode(), toString(), copy()
+data class Fruit(
+    val name: String,
+    val emoji: String
+)
